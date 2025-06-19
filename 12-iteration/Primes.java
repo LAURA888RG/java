1ec88cc647c5306245797cd9d9a4cd5f713a417c
laura888rg <rodrilaura220@gmail.com>
2025-06-19 09:55:27 +0200

    subir

--- 12-iteration/Primes.java
+++ 12-iteration/Primes.java
@@ -3,32 +3,30 @@ import java.util.Scanner;
 public class Primes {
 
     private static int getDataByScanner() {
-    Scanner scanner  = new Scanner(System.in);
-    System.out.println("Dime un numero para ver si es primo");
-    int number = scanner.nextInt();
-    System.out.println("");
-    scanner.close();
-    return number;
-}
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dime un número para ver si es primo");
+        int number = scanner.nextInt();
+        System.out.println("");
+        scanner.close();
+        return number;
+    }
 
-private static boolean isPrime(int number) {
-    for (int i = 2; i < number; i++) {
-        if (number % i == 0) {
-        return false; 
+    public static boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-    } 
-    return true;
-}
-
-}
-
-public static void main(String[] args){
-    int number = getDataByScanner();
-
-    String message = isPrime(number)
-          ? "%s es primo" 
-          : "%s no es primo";
-    System.out.printf(message, number);
-}
+    public static void main(String[] args) {
+        int number = getDataByScanner();
+        String message = isPrime(number)
+                ? "%s SI es primo"
+                : "%s NO es primo";
+        System.out.printf(message, number);
+    }
 
 }
+
---
+++ 15-ejercicios/Factorial.java
@@ -0,0 +1,35 @@
+public class Factorial {
+
+    static long calculateFactorial(long number) {
+        if (number < 0) {
+            number = number * -1;
+        }
+
+        long result = 1;
+
+        for (long i = number; i > 0; i--) {
+            result = result * i;
+            // result *= i;   
+        }
+        return result;
+
+} 
+
+static long recursiveFactorial(long number) {
+    if (number == 1) {
+        return number;
+    }
+
+     Long result = number * recursiveFactorial(number-1);
+     return result;
+
+}
+
+ public static void main(String[] args) {
+
+    System.out.println(calculateFactorial(10));
+    System.out.println(calculateFactorial(10));
+        
+}
+    
+}
